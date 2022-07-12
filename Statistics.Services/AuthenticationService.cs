f8195b072b7ea268f1c4c74bd4de20e549e20646
DanielXOO <dankulinkovich@gmail.com>
2022-07-12 12:50:28 +0300

    Add client credentials authentication

---
+++ Statistics.Services/AuthenticationService.cs
@@ -0,0 +1,30 @@
+﻿using Statistics.Data.Refit;
+using Statistics.Services.Abstracts;
+using SurveyMe.AuthenticationApi.Models.Request;
+using SurveyMe.Common.Exceptions;
+
+namespace Statistics.Services;
+
+public class AuthenticationService : IAuthenticationService
+{
+    private readonly IAuthenticationApi _authenticationApi;
+    
+    
+    public AuthenticationService(IAuthenticationApi authenticationApi)
+    {
+        _authenticationApi = authenticationApi;
+    }
+    
+    
+    public async Task<string> GetTokenAsync(GetTokenRequestModel request)
+    {
+        if (request == null)
+        {
+            throw new BadRequestException("Request model is empty");
+        }
+        
+        var response = await _authenticationApi.GetTokenAsync(request);
+
+        return response.access_token;
+    }
+}
